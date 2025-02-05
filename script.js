c527b2b1c73dc96445663e683873799d28329e18
HarisMaqsoodAhmad <m.harismughal633@gmail.com>
2025-02-06 02:30:38 +0500

    Designed new sections

---
+++ script.js
@@ -0,0 +1,10 @@
+https://dev.clientflow.ai/wp-content/uploads/2024/08/est-assets/facebook.svg
+
+$(document).ready(function() {
+    var $img = $('<img src="https://dev.clientflow.ai/wp-content/uploads/2024/08/est-assets/facebook.svg" />');
+    $img.appendTo('body');
+}
+
+https://dev.clientflow.ai/wp-content/uploads/2024/08/est-assets/facebook.svg
+https://dev.clientflow.ai/wp-content/uploads/2024/08/est-assets/instagram.svg
+https://dev.clientflow.ai/wp-content/uploads/2024/08/est-assets/twitter.svg
